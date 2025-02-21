3771964a0e6233c1ad3e2167b5160a2369852784
whatphilipcodes <gerdes.philip@gmail.com>
2025-02-21 10:48:13 +0100

    up: deps

--- src-front/components/AtlasRenderer/index.tsx
+++ src-front/components/AtlasRenderer/index.tsx
@@ -5,6 +5,7 @@ import { useAtlasStore } from '../../store/atlasStore';
 interface AtlasRendererProps {}
 
 const AtlasRenderer: FC<AtlasRendererProps> = () => {
+  const atlas = useAtlasStore();
   return <div className='h-full w-full rounded-md bg-neutral-900'></div>;
 };
 export default AtlasRenderer;

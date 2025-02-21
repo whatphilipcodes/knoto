452f1d969eb26fa94f37411221e6b2323ffe5006
whatphilipcodes <gerdes.philip@gmail.com>
2025-02-21 13:06:19 +0100

    add: to-do

--- src-front/store/applicationStore.ts
+++ src-front/store/applicationStore.ts
@@ -21,6 +21,8 @@ type ApplicationState = typeof defaultApplicationState & {
   openAtlasDir: () => Promise<void>;
 };
 
+// to-do: handle case where atlas is saved in config but folder has moved or been deleted
+
 // Create your stores using the typed middleware
 export const useApplicationStore = create<ApplicationState>()(
   subscribeWithSelector(
